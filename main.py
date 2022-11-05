fba7e1c4de55fe01395dfcc0be15f08c5221b194
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-05 15:54:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ async def help(ctx: interactions.CommandContext):
         description="""1. **How to report**"""
     )],components=[interactions.Button(
         style=interactions.ButtonStyle.PRIMARY,
-        emoji={"name":"arrow_backward"},
+        emoji="◀️",
         custom_id="previous",
     ),interactions.Button(
         style=interactions.ButtonStyle.PRIMARY,
-        emoji={"name":"arrow_forward"},
+        emoji="▶️",
         custom_id="next",
     )])
 
