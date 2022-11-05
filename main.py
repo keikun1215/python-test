d1a933f5a104776afb33ead69a9f901e55b5dfa7
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-05 12:35:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import interactions, os
 bot = interactions.Client(token=os.getenv("token"))
+
+@bot.command(
+    name="test",
+    description="Test command"
+)
+async def my_first_command(ctx: interactions.CommandContext):
+    await ctx.send("Hi there!")
+
 bot.start()
