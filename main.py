7bfe9d082a63eac1815e54aea952b18b7eda7487
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-06 10:47:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def help(ctx: interactions.CommandContext):
         placeholder="CLICK ME",
         custom_id="helpmenu",
     )])
-@bot.component("hm_htr")
+@bot.selectmenu("hm_htr")
 async def selectmenu_response(ctx):
     await ctx.send("test", ephemeral=True)
 
