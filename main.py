3c80be3f397f4fb5e97ca8b5ca3b29bdb548bac7
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-11-06 10:31:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,8 @@ async def help(ctx: interactions.CommandContext):
         placeholder="CLICK ME",
         custom_id="helpmenu",
     )])
+@bot.component("hm_htr")
+async def button_response(ctx):
+    await ctx.send("test", ephemeral=True)
 
 bot.start()
